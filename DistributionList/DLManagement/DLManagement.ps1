8c0e34ee0c08e7018013ff6497bf23edc275d178
radamanthys90 <72862368+radamanthys90@users.noreply.github.com>
2025-01-09 17:55:42 +0100

    Create DLManagement.ps1

---
+++ DistributionList/DLManagement/DLManagement.ps1
@@ -0,0 +1,31 @@
+# Connect to Exchange Online
+connect-ExchangeOnline -Credential $credential
+
+# Specify the path to the input CSV file
+$csvPath = "C:\Path\To\Your\CSV\File.csv"
+
+# Import the DL details from the CSV file
+$dlDetails = Import-Csv -Path $csvPath
+
+# Iterate through each DL in the CSV file and create or delete the DL
+foreach ($dl in $dlDetails) {
+    try {
+        if ($dl.Action -eq "Create") {
+            # Create the DL
+            New-DistributionGroup -Alias $dl.Alias -Name $dl.Name -PrimarySmtpAddress $dl.PrimarySmtpAddress -ErrorAction Stop
+            Write-Host "Created DL with Alias: $($dl.Alias)." -ForegroundColor Green
+        }
+        elseif ($dl.Action -eq "Delete") {
+            # Delete the DL
+            Remove-DistributionGroup -Identity $dl.Alias -Confirm:$false -ErrorAction Stop
+            Write-Host "Deleted DL with Alias: $($dl.Alias)." -ForegroundColor Green
+        }
+        else {
+            Write-Host "Unrecognized action for Alias: $($dl.Alias): $($dl.Action)." -ForegroundColor Yellow
+        }
+    } catch {
+        Write-Host "Error executing action for DL with Alias: $($dl.Alias): $($_.Exception.Message)" -ForegroundColor Red
+    }
+}
+
+Write-Host "Operation completed."

de516962bd231d9462818006bc0a0fb402e5b8e0
radamanthys90 <72862368+radamanthys90@users.noreply.github.com>
2025-01-09 18:04:13 +0100

    Update AddUserToDL.ps1

--- DistributionList/AddUserToDL/AddUserToDL.ps1
+++ DistributionList/AddUserToDL/AddUserToDL.ps1
@@ -1,28 +1,39 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$CredentialPath,
+
+    [Parameter(Mandatory = $true)]
+    [string]$DLAlias,
+
+    [Parameter(Mandatory = $true)]
+    [string]$CSVPath,
+
+    [Parameter(Mandatory = $true)]
+    [string]$OutputCSVPath
+)
+
+# Import credentials
+$credential = Import-Clixml -Path $CredentialPath
+
 # Connect to Exchange Online
-connect-ExchangeOnline -Credential $credential
-
-# Specify the alias of the distribution list (DL)
-$dlAlias = "yourDLAlias"
-
-# Specify the path of the input CSV file
-$csvPath = "C:\Path\To\Your\CSV\File.csv"
+Connect-ExchangeOnline -Credential $credential
 
 # Import users from the CSV file
-$users = Import-Csv -Path $csvPath
+$users = Import-Csv -Path $CSVPath
 
 # Iterate through each user and add them to the DL
 foreach ($user in $users) {
     try {
         if ($user.UPN) {
             # Add the user to the DL using the UPN
-            Add-DistributionGroupMember -Identity $dlAlias -Member $user.UPN -ErrorAction Stop
+            Add-DistributionGroupMember -Identity $DLAlias -Member $user.UPN -ErrorAction Stop
             Write-Host "Added $($user.UPN) to the DL using UPN." -ForegroundColor Green
         }
         elseif ($user.sAMAccountName) {
             # Get the email address using the sAMAccountName
             $email = (Get-User -Identity $user.sAMAccountName -ErrorAction Stop).PrimarySmtpAddress
             # Add the user to the DL using the email address
-            Add-DistributionGroupMember -Identity $dlAlias -Member $email -ErrorAction Stop
+            Add-DistributionGroupMember -Identity $DLAlias -Member $email -ErrorAction Stop
             Write-Host "Added $($user.sAMAccountName) to the DL using sAMAccountName." -ForegroundColor Green
         }
         else {
@@ -34,12 +45,9 @@ foreach ($user in $users) {
 }
 
 # Retrieve the updated list of DL members
-$dlMembers = Get-DistributionGroupMember -Identity $dlAlias
-
-# Specify the path of the output CSV file
-$outputCsvPath = "C:\Path\To\Your\Output\File.csv"
+$dlMembers = Get-DistributionGroupMember -Identity $DLAlias
 
 # Export the updated list of members to a CSV file
-$dlMembers | Select-Object DisplayName, PrimarySmtpAddress | Export-Csv -Path $outputCsvPath -NoTypeInformation
+$dlMembers | Select-Object DisplayName, PrimarySmtpAddress | Export-Csv -Path $OutputCSVPath -NoTypeInformation
 
-Write-Host "Updated list of DL members exported to $outputCsvPath."
+Write-Host "Updated list of DL members exported to $OutputCSVPath."
